6a063f265d4b983ab1d1e7e555a685ac445a67aa
Fabricio Henrique da Silva <fabricionhsilva@gmail.com>
2023-01-08 19:04:34 -0300

    feat: global settings for project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListarCredoresComponent } from './components/credor/listar-credores/listar-credores.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'listar',
+  },
+  {
+    path: 'pessoas/listarPessoas',
+    component: ListarCredoresComponent
+  }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
