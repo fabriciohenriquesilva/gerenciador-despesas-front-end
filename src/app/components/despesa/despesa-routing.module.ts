38eb51ccca72b909ffcb141327eb2b6c301afc3d
Fabricio Henrique da Silva <fabricionhsilva@gmail.com>
2023-03-09 22:24:13 -0300

    fix: despesa navigation between child routes

--- src/app/components/despesa/despesa-routing.module.ts
+++ src/app/components/despesa/despesa-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { CadastrarDepesaComponent } from './cadastrar-depesa/cadastrar-depesa.component';
 import { DespesaComponent } from './despesa.component';
 import { EditarDespesaComponent } from './editar-despesa/editar-despesa.component';
-
 import { ListarDespesasComponent } from './listar-despesas/listar-despesas.component';
 
 const despesaRoutes: Routes = [
   { path: 'despesas', component: DespesaComponent, children: [
-    { path: 'listar', component: ListarDespesasComponent },
+    { path: '', component: ListarDespesasComponent },
     { path: 'cadastro', component: CadastrarDepesaComponent },
     { path: ':id', component: EditarDespesaComponent }
   ]}
