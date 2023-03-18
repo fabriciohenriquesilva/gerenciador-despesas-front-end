8c0c655a6448c6f74f79338be901490ea31ddcc1
Fabricio Henrique da Silva <fabricionhsilva@gmail.com>
2023-03-18 12:44:32 -0300

    feat: input field reusable component

--- src/app/components/despesa/despesa.module.ts
+++ src/app/components/despesa/despesa.module.ts
@@ -9,6 +9,7 @@ import { NavigationModule } from '../navigation/navigation.module';
 import { CadastrarDepesaComponent } from './cadastrar-depesa/cadastrar-depesa.component';
 import { DespesaComponent } from './despesa.component';
 import { EditarDespesaComponent } from './editar-despesa/editar-despesa.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { EditarDespesaComponent } from './editar-despesa/editar-despesa.componen
     FormsModule,
     ReactiveFormsModule,
     DespesaRoutingModule,
-    NavigationModule
+    NavigationModule,
+    SharedModule
   ],
   providers: [
     DespesaService,
