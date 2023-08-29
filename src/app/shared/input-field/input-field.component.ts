85301e91067d53fa238f6c6cf9e552da936e61e9
Fabricio Henrique da Silva <fabricionhsilva@gmail.com>
2023-08-29 17:24:45 -0300

    feat: melhorias no componente personalizado de input

--- src/app/shared/input-field/input-field.component.ts
+++ src/app/shared/input-field/input-field.component.ts
@@ -1,16 +1,16 @@
-import { Component, forwardRef, Input } from '@angular/core';
-import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import {Component, forwardRef, Input} from '@angular/core';
+import {ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR} from '@angular/forms';
 
 const INPUT_FIELD_VALUE_ACESSOR: any = {
   provide: NG_VALUE_ACCESSOR,
-  useExisting: forwardRef( () => InputFieldComponent ),
+  useExisting: forwardRef(() => InputFieldComponent),
   multi: true
 }
 
 @Component({
   selector: 'app-input-field',
   templateUrl: './input-field.component.html',
-  styleUrls: ['./input-field.component.css'],
+  styleUrls: ['./input-field.component.scss'],
   providers: [INPUT_FIELD_VALUE_ACESSOR]
 })
 export class InputFieldComponent implements ControlValueAccessor {
@@ -20,6 +20,8 @@ export class InputFieldComponent implements ControlValueAccessor {
   @Input() placeholder = '';
   @Input() id!: string;
   @Input() isReadOnly = false;
+  @Input() control: any | FormControl;
+  @Input() optional: boolean = false;
 
   private innerValue: any;
 
@@ -28,19 +30,22 @@ export class InputFieldComponent implements ControlValueAccessor {
   }
 
   set value(v: any) {
-    if(v !== this.innerValue) {
+    if (v !== this.innerValue) {
       this.innerValue = v;
       this.onChangeCb(v);
     }
   }
 
-  constructor() { }
+  constructor() {
+  }
 
-  onChangeCb: (_: any) => void = () => {};
-  onTouchedCb: (_: any) => void = () => {};
+  onChangeCb: (_: any) => void = () => {
+  };
+  onTouchedCb: (_: any) => void = () => {
+  };
 
   writeValue(v: any): void {
-    if(v !== this.innerValue) {
+    if (v !== this.innerValue) {
       this.innerValue = v;
       this.onChangeCb(v);
     }
@@ -56,6 +61,20 @@ export class InputFieldComponent implements ControlValueAccessor {
 
   setDisabledState?(isDisabled: boolean): void {
     this.isReadOnly = isDisabled;
+  }
+
+  getLabel(): string {
+    if (this.optional) {
+      return this.label + " (opcional)"
+    }
+    return this.label;
+  }
+
+  hasError(): string {
+    if (this.control?.errors?.['required'] && this.control?.touched) {
+      return "has-error";
+    }
+    return "";
   }
 
 }
