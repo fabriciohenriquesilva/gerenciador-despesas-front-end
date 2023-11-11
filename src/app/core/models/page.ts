8a54b7681d243628c243ea8342e558c8e63efd14
Fabricio Henrique da Silva <fabricionhsilva@gmail.com>
2023-11-11 20:13:41 -0300

    fix: arquivo em diretório incorreto

---
+++ src/app/core/models/page.ts
@@ -0,0 +1,11 @@
+export interface Page<T> {
+    content: T[];
+    totalElements: number;
+    last: boolean;
+    totalPages: number;
+    size: number;
+    number: number;
+    numberOfElements: number;
+    first: boolean;
+    empty: boolean;
+}
