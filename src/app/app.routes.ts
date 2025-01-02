2c58451eb6fe01bd2ff8d1ecda28c58d375387a8
Fabricio Henrique da Silva <fabricionhsilva@gmail.com>
2025-01-02 10:33:19 -0300

    wip: página de login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,23 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
+import {Routes} from '@angular/router';
+import {HomeComponent} from './pages/home/home.component';
+import {LoginComponent} from "./pages/user/login/login.component";
+import {AuthGuard} from "./core/services/auth.guard.service";
 
 export const routes: Routes = [
     {
-        path: '',
-        component: HomeComponent
+        path: 'auth/login',
+        component: LoginComponent,
+    },
+    {
+        path: 'home',
+        component: HomeComponent,
+        canActivate: [AuthGuard]
     },
     {
         path: 'categorias',
         loadChildren: () => import("./pages/categoria/categoria-routing.module")
-            .then(m => m.CategoriaRoutingModule)
+            .then(m => m.CategoriaRoutingModule),
+        canActivate: [AuthGuard]
     }
     // {
     //     path: 'cadastros',
---
+++ src/app/pages/user/dto/user.login.dto.ts
@@ -0,0 +1,7 @@
+export class UserLoginDto {
+
+    username!: string;
+
+    password!: string;
+
+}
